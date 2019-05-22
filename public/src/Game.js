1973cbfc705fa83d6baf8df4d1ff47eeadcdadb5
sajrus33 <brianwala22@gmail.com>
2019-05-22 02:23:49 +0200

    bug fix counter++ wrong
    counter++ wrong, it was counting on click on done cards.

--- public/src/Game.js
+++ public/src/Game.js
@@ -9,176 +9,163 @@ import { Interfejs } from "/game-memory-cards/public/src/Interfejs.js";
 
 console.log({ utilities, myAlert, Card, resources, Table });
 
-
 export class Game {
-    constructor(cardsOption = 1, category = "catedras") {
-        this.category = category;
-        myAlert(this.category);
+  constructor(cardsOption = 1, category = "catedras") {
+    this.category = category;
+    myAlert(this.category);
 
-        this.cardsOption = cardsOption;
-        this.resources = resources;
-        this.utilities = utilities;
+    this.cardsOption = cardsOption;
+    this.resources = resources;
+    this.utilities = utilities;
 
-        this.Interfejs = Interfejs;
+    this.Interfejs = Interfejs;
 
-        this.gameWrapper = document.createElement("div");
-        this.gameWrapper.style.width = "100%";
-        this.gameWrapper.style.height = "100%";
-        this.gameWrapper.style.minWidth = "320px";
-        this.gameWrapper.style.minHeight = "320px";
+    this.gameWrapper = document.createElement("div");
+    this.gameWrapper.style.width = "100%";
+    this.gameWrapper.style.height = "100%";
+    this.gameWrapper.style.minWidth = "320px";
+    this.gameWrapper.style.minHeight = "320px";
 
+    this.gameSize = size => {
+      this.gameWrapper.style.width = size;
+      this.gameWrapper.style.height = size;
+    };
 
+    document.body.appendChild(this.gameWrapper);
 
-        this.gameSize = (size) => {
-            this.gameWrapper.style.width = size;
-            this.gameWrapper.style.height = size;
-        };
+    this.returnBtn = document.createElement("button");
+    this.returnBtn.classList.add("returnBtn");
+    this.returnBtn.addEventListener("click", () => {
+      this.finish();
+    });
+    this.gameWrapper.appendChild(this.returnBtn);
 
+    this.table = new Table(
+      this.gameWrapper,
+      "100%",
+      "100%",
+      this.resources.imgs[this.category].category,
+      this.resources.imgs[this.category].table
+    );
+    this.Card = Card;
+    this.cards = [];
+    this.cardsChecked = [];
+    this.needUncheck = false;
+    this.run = false;
 
+    this.uncheckCards = () => {
+      this.cards.forEach(card => {
+        if (card.checked) {
+          card.check();
+        }
+      });
+      this.needUncheck = false;
+      this.table.statistics.chance--;
+    };
 
-        document.body.appendChild(this.gameWrapper);
+    // eventListener for cards manipulation
+    this.result = () => {
+      if (!this.run) {
+        this.run = true;
+        this.table.statistics.timerStart();
+      }
+      // this.table.statistics.chance++;
+      if (this.needUncheck) {
+        this.uncheckCards();
+      }
+      // two cards to compare
+      let first, second;
+      // one chance ++
 
-        this.returnBtn = document.createElement("button");
-        this.returnBtn.classList.add("returnBtn");
-        this.returnBtn.addEventListener("click", () => {
-            this.finish();
-        });
-        this.gameWrapper.appendChild(this.returnBtn);
+      //with card is checked ?
+      this.cards.forEach((card, i) => {
+        if (card.checked) {
+          this.table.statistics.chance++;
 
+          this.cardsChecked.push(card);
+          if (first != undefined) {
+            second = i;
+          } else first = i;
+        }
+      });
+      if (this.cardsChecked.length == 2) {
+        // if those two cards are equal
+        if (this.cardsChecked[0].imgTrue == this.cardsChecked[1].imgTrue) {
+          console.log("similar");
+          this.cards[first].done();
+          this.cards[second].done();
+          this.table.statistics.score++;
 
-        this.table = new Table(this.gameWrapper, "100%", "100%", this.resources.imgs[this.category].category, this.resources.imgs[this.category].table);
-        this.Card = Card;
-        this.cards = [];
-        this.cardsChecked = [];
-        this.needUncheck = false;
-        this.run = false;
+          if (this.table.statistics.score == this.cards.length / 2) {
+            this.table.statistics.timerStop();
+          }
+        } else {
+          this.needUncheck = true;
+          console.log("not similar");
+        }
+        // anyway
+        first = undefined;
+        second = undefined;
+      }
+      this.cardsChecked = [];
+    };
 
+    this.setCards = (y = 0) => {
+      for (let i = 0; i < resources.cards.number[y]; i++) {
+        this.cards.push(
+          new Card(
+            this.table.cardswrapper,
+            this.resources.cards.width[y],
+            this.resources.cards.height[y],
+            this.resources.imgs[this.category].faces[0],
+            this.resources.imgs[this.category].back
+          )
+        );
+      }
+      // random img
+      let randomNumbers = [];
+      const max = this.cards.length - 1;
 
+      for (let y = 0; y < 2; y++) {
+        for (let i = 0; i <= max / 2; i++) {
+          randomNumbers.push(i);
+        }
+      }
 
-        this.uncheckCards = () => {
-            this.cards.forEach((card) => {
+      let currentIndex = randomNumbers.length;
+      let temporaryValue, randomIndex;
 
-                if (card.checked) {
-                    card.check();
-                }
-            });
-            this.needUncheck = false;
-            this.table.statistics.chance--;
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
+        // And swap it with the current element.
+        temporaryValue = randomNumbers[currentIndex];
+        randomNumbers[currentIndex] = randomNumbers[randomIndex];
+        randomNumbers[randomIndex] = temporaryValue;
+      }
 
-        };
+      console.log(randomNumbers);
+      // now update, all random pairs of imgs
+      for (let i = 0; i <= max; i++) {
+        this.cards[i].load(
+          this.resources.imgs[this.category].faces[randomNumbers[i]]
+        );
+      }
+    };
+    this.finish = () => {
+      this.gameWrapper.remove();
+      new this.Interfejs();
+    };
 
-        // eventListener for cards manipulation
-        this.result = () => {
-            if (!this.run) {
-                this.run = true;
-                this.table.statistics.timerStart();
-            }
-            this.table.statistics.chance++;
-            if (this.needUncheck) {
-                this.uncheckCards();
-            }
-            // two cards to compare
-            let first, second;
-            // one chance ++
-
-            //with card is checked ?
-            this.cards.forEach((card, i) => {
-                if (card.checked) {
-
-                    this.cardsChecked.push(card);
-                    if (first != undefined) {
-                        second = i;
-                    } else first = i;
-
-                }
-
-            });
-            if (this.cardsChecked.length == 2) {
-                // if those two cards are equal
-                if (this.cardsChecked[0].imgTrue == this.cardsChecked[1].imgTrue) {
-                    console.log("similar");
-                    this.cards[first].done();
-                    this.cards[second].done();
-                    this.table.statistics.score++;
-
-                    if (this.table.statistics.score == (this.cards.length / 2)) {
-                        this.table.statistics.timerStop();
-                    }
-
-
-                } else {
-                    this.needUncheck = true;
-                    console.log("not similar");
-                }
-                // anyway
-                first = undefined;
-                second = undefined;
-
-            }
-            this.cardsChecked = [];
-        };
-
-        this.setCards = (y = 0) => {
-
-            for (let i = 0; i < resources.cards.number[y]; i++) {
-                this.cards.push(new Card(this.table.cardswrapper, this.resources.cards.width[y], this.resources.cards.height[y], this.resources.imgs[this.category].faces[0], this.resources.imgs[this.category].back));
-            }
-            // random img
-            let randomNumbers = [];
-            const max = this.cards.length - 1;
-
-            for (let y = 0; y < 2; y++) {
-                for (let i = 0; i <= max / 2; i++) {
-                    randomNumbers.push(i);
-
-                }
-            }
-
-
-
-
-
-            let currentIndex = randomNumbers.length;
-            let temporaryValue, randomIndex;
-
-            // While there remain elements to shuffle...
-            while (0 !== currentIndex) {
-                // Pick a remaining element...
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex -= 1;
-
-                // And swap it with the current element.
-                temporaryValue = randomNumbers[currentIndex];
-                randomNumbers[currentIndex] = randomNumbers[randomIndex];
-                randomNumbers[randomIndex] = temporaryValue;
-            }
-
-            console.log(randomNumbers);
-            // now update, all random pairs of imgs
-            for (let i = 0; i <= max; i++) {
-                this.cards[i].load(this.resources.imgs[this.category].faces[randomNumbers[i]]);
-            }
-
-        };
-        this.finish = () => {
-            this.gameWrapper.remove();
-            new this.Interfejs();
-
-        };
-
-        this.init = (cardsOption = this.cardsOption) => {
-            this.table.appendTo();
-            this.setCards(cardsOption);
-            this.cards.forEach(card => {
-                card.canvas.addEventListener("click", this.result);
-            });
-
-        };
-    }
+    this.init = (cardsOption = this.cardsOption) => {
+      this.table.appendTo();
+      this.setCards(cardsOption);
+      this.cards.forEach(card => {
+        card.canvas.addEventListener("click", this.result);
+      });
+    };
+  }
 }
-
-
-
-
-
--- public/src/myAlert.js
+++ public/src/myAlert.js
@@ -1,13 +1,13 @@
-// my own alert() function 
+// my own alert() function
 export function myAlert(describe) {
-    const oldAlert = document.querySelector(".tip");
-    if (oldAlert) {
-        oldAlert.remove();
-    }
-    const newAlert = document.createElement("div");
-    newAlert.classList.add("tip");
-    newAlert.innerText = String(describe);
-    document.body.appendChild(newAlert);
+  const oldAlert = document.querySelector(".tip");
+  if (oldAlert) {
+    oldAlert.remove();
+  }
+  const newAlert = document.createElement("div");
+  newAlert.classList.add("tip");
+  newAlert.innerText = String(describe);
+  document.body.appendChild(newAlert);
 }
 
 // .tip {
